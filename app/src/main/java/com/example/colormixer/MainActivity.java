bb5ae3faec0e908f401d1e9c7b67b75db7b6cd5d
Your Name <you@example.com>
2020-03-18 18:11:21 -0400

    march 18

---
+++ app/src/main/java/com/example/colormixer/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.colormixer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView barLabel;
+    private SeekBar redBar;
+    private SeekBar greenBar;
+    private SeekBar blueBar;
+    private View view;
+    private int color = 0xFF000000;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        barLabel =findViewById(R.id.View);
+        redBar = findViewById(R.id.redBar);
+        greenBar = findViewById(R.id.greenBar);
+        blueBar = findViewById(R.id.blueBar);
+
+        redBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                barLabel.setText(progress+"");
+                color = (color&0xFFFF00FF)+(progress*0*100);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        greenBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                color = (color&0xFFFF00FF)+(progress*0x100);
+                view.setBackgroundColor(color);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        blueBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                color = (color&0xFFFFFF00)+progress;
+                view.setBackgroundColor(color);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+}
